d890f3189c1ec8fa7e92cf49513ac104653c6bb3
Vladislavs Dovgalecs <vladislavsd@voicebox.com>
2017-04-12 14:29:19 -0700

    Add project files.

---
+++ LearnGaussianWithMissingData/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrosoftResearch.Infer;
+using MicrosoftResearch.Infer.Models;
+using MicrosoftResearch.Infer.Maths;
+
+namespace LearnGaussianWithMissingData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rand.Restart(5209);     // www.random.org
+            Random rnd = new Random(398);   // ww.random.org
+
+            // data points
+            double[] data = new double[] { -1, 0.1, 0.3, -1, -0.2, 0.05, -0.05, -1, 0.05, 0.01 };
+            bool[] isMissing = new bool[] { true, false, false, true, false, false, false, true, false, false };
+            
+            Range r = new Range(data.Length);
+
+            // create mean & precision RVs
+            Variable<double> mean = Variable.GaussianFromMeanAndVariance(0.0, 10.0).Named("mean");
+            Variable<double> precision = Variable.GammaFromShapeAndScale(1.0, 1.0).Named("precision");
+
+            // create observed RV array for data
+            VariableArray<double> x = Variable.Array<double>(r);
+            //x[r] = Variable.GaussianFromMeanAndPrecision(mean, precision).ForEach(r);
+            x.ObservedValue = data;
+
+            // create missing data indicator RV array
+            VariableArray<bool> missing = Variable.Observed(isMissing, r);
+
+            using (Variable.ForEach(r))
+            {
+                using (Variable.IfNot(missing[r]))
+                {
+                    x[r] = Variable.GaussianFromMeanAndPrecision(mean, precision);
+                }
+            }
+
+            InferenceEngine engine = new InferenceEngine();
+
+            Console.WriteLine( "mean posterior = " + engine.Infer(mean) );
+            Console.WriteLine( "precision posterior = " + engine.Infer(precision) );
+
+            Console.WriteLine("\nPress any key...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LearnGaussianWithMissingData/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LearnGaussianWithMissingData")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LearnGaussianWithMissingData")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b4ad0ae1-eb3a-4024-962d-14a08eba3cdd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
