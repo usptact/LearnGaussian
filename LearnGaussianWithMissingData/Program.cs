39786c47d773b09606e35467cc0ed94bac02b605
Vladislavs Dovgalecs <vladislavsd@speak.ai>
2018-10-08 18:17:34 -0700

    Updated to use open source Infer.NET

--- LearnGaussianWithMissingData/Program.cs
+++ LearnGaussianWithMissingData/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MicrosoftResearch.Infer;
-using MicrosoftResearch.Infer.Models;
-using MicrosoftResearch.Infer.Maths;
+using Microsoft.ML.Probabilistic.Models;
+using Microsoft.ML.Probabilistic.Math;
 
 namespace LearnGaussianWithMissingData
 {
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Infer.NET demo: Learn a Gaussian from dataset with missing values.
 
 Shows how to learn parameters of a Gaussian random variable (mean and precision) given data that contains missing values.
+
+Updated to use open source Infer.NET.
